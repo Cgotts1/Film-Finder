037ab7867384277ef55bff2eb2a141488ae36f22
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-11-08 20:54:37 -0800

    added background image

--- assets/js/results.js
+++ assets/js/results.js
@@ -13,21 +13,21 @@ var movieTitle = localStorage.getItem("selectedMovie")
         movieImage.src = localStorage.getItem("selectedPoster")
         movieImage.classList = "w-100 h-100 rounded-5 py-3"
         moviePosterResults.append(movieImage)
-
+        console.log(result)
         let movieListEl = `<ul class = "fs-3">
-                            <li>Actors: ${result.Actors !== undefined ? result.Actors : "Sorry"}
-                            <li>Director: ${result.Director !== undefined ? result.Director : "For"}
-                            <li>Writers: ${result.Writer !== undefined ? result.Writer : "The"}
-                            <li>Release Date: ${result.Released !== undefined ? result.Released : "Inconvenience"}
-                            <li>Movie Rating: ${result.Rated !== undefined ? result.Rated : "No"}
-                            <li>Genre: ${result.Genre !== undefined ? result.Genre : "Movie"}
-                            <li>Runtime: ${result.Runtime !== undefined ? result.Runtime : "Information"}
-                            <li>Plot Summary: ${result.Plot !== undefined ? result.Plot : "Available"}
+                            <li>Actors: ${result.Actors !== undefined ? result.Actors : "Actors information unavailable"}
+                            <li>Director: ${result.Director !== undefined ? result.Director : "Directors information unavailable"}
+                            <li>Writers: ${result.Writer !== undefined ? result.Writer : "Writers information unavailable"}
+                            <li>Release Date: ${result.Released !== undefined ? result.Released : "Release date information unavailable"}
+                            <li>Movie Rating: ${result.Rated !== undefined ? result.Rated : "Movie rating information unavailable"}
+                            <li>Genre: ${result.Genre !== undefined ? result.Genre : "Genre information unavailable"}
+                            <li>Runtime: ${result.Runtime !== undefined ? result.Runtime : "Runtime information unavailable"}
+                            <li>Plot Summary: ${result.Plot !== undefined ? result.Plot : "Plot information unavailable"}
                             <p>
                             <p>Ratings:
-                            <li>${result.Ratings !== undefined ? result.Ratings[0].Source :"Please"} ${result.Ratings !== undefined ? result.Ratings[0].Value : ""}
-                            <li>${result.Ratings !== undefined ? result.Ratings[1].Source :"Try"} ${result.Ratings !== undefined ? result.Ratings[1].Value : ""}
-                            <li>${result.Ratings !== undefined ? result.Ratings[2].Source :"Google.com"} ${result.Ratings !== undefined ? result.Ratings[2].Value : ""}
+                            <li>${result.Ratings === undefined ? "IMDB rating unavailable": result.Ratings[0].Source} ${result.Ratings === undefined ? "" : result.Ratings[0].Value}
+                            <li>${result.Ratings === undefined ? "Rotten Tomatoes rating unavailable" : result.Ratings[1].Source} ${result.Ratings === undefined ? "" : result.Ratings[1].Value}
+                            <li>${result.Ratings === undefined ? "Metacritic rating unavailable" : result.Ratings[2].Source} ${result.Ratings === undefined ? "" : result.Ratings[2].Value}
                             <ul>`
         
         omdbInfo.innerHTML = movieListEl               
